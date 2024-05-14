1029aa48d0231e7d589714ace2bbe69cfb1c3908
Meneer-de-Bruin <60819139+Meneer-de-Bruin@users.noreply.github.com>
2024-05-14 10:04:23 +0200

    Update conf.py

--- webcurator-docs/conf.py
+++ webcurator-docs/conf.py
@@ -62,7 +62,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '/guides/apis/descriptions']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'guides/apis/descriptions']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
